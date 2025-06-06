ff83816b9309149f21477fbb95af44187b8cea62
elturanvalizada <elturanvelizade@gmail.com>
2025-06-06 21:41:45 +0200

    Initial commit with solution and unit tests

---
+++ QuadraticLib/QuadraticSolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace QuadraticLib
+{
+    public class QuadraticSolver
+    {
+        public static (int, double?, double?) Solve(double a, double b, double c)
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+                return (0, null, null);
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+                return (1, root, null);
+            }
+            else
+            {
+                double sqrtDisc = Math.Sqrt(discriminant);
+                double root1 = (-b + sqrtDisc) / (2 * a);
+                double root2 = (-b - sqrtDisc) / (2 * a);
+                return (2, root1, root2);
+            }
+        }
+    }
+}
+
---
+++ QuadraticTests/QuadraticSolverTests.cs
@@ -0,0 +1,10 @@
+﻿namespace QuadraticTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
